36832bb2fad7fab1f361d19b8c380b0a24623e4e
David Drumond <2181042@my.ipleiria.pt>
2019-11-26 10:23:45 +0000

    First commit

---
+++ main.c
@@ -0,0 +1,255 @@
+/**
+  Generated Main Source File
+
+  Company:
+    Microchip Technology Inc.
+
+  File Name:
+    main.c
+
+  Summary:
+    This is the main file generated using PIC10 / PIC12 / PIC16 / PIC18 MCUs
+
+  Description:
+    This header file provides implementations for driver APIs for all modules selected in the GUI.
+    Generation Information :
+        Product Revision  :  PIC10 / PIC12 / PIC16 / PIC18 MCUs - 1.77
+        Device            :  PIC18F45K22
+        Driver Version    :  2.00
+*/
+
+/*
+    (c) 2018 Microchip Technology Inc. and its subsidiaries. 
+    
+    Subject to your compliance with these terms, you may use Microchip software and any 
+    derivatives exclusively with Microchip products. It is your responsibility to comply with third party 
+    license terms applicable to your use of third party software (including open source software) that 
+    may accompany Microchip software.
+    
+    THIS SOFTWARE IS SUPPLIED BY MICROCHIP "AS IS". NO WARRANTIES, WHETHER 
+    EXPRESS, IMPLIED OR STATUTORY, APPLY TO THIS SOFTWARE, INCLUDING ANY 
+    IMPLIED WARRANTIES OF NON-INFRINGEMENT, MERCHANTABILITY, AND FITNESS 
+    FOR A PARTICULAR PURPOSE.
+    
+    IN NO EVENT WILL MICROCHIP BE LIABLE FOR ANY INDIRECT, SPECIAL, PUNITIVE, 
+    INCIDENTAL OR CONSEQUENTIAL LOSS, DAMAGE, COST OR EXPENSE OF ANY KIND 
+    WHATSOEVER RELATED TO THE SOFTWARE, HOWEVER CAUSED, EVEN IF MICROCHIP 
+    HAS BEEN ADVISED OF THE POSSIBILITY OR THE DAMAGES ARE FORESEEABLE. TO 
+    THE FULLEST EXTENT ALLOWED BY LAW, MICROCHIP'S TOTAL LIABILITY ON ALL 
+    CLAIMS IN ANY WAY RELATED TO THIS SOFTWARE WILL NOT EXCEED THE AMOUNT 
+    OF FEES, IF ANY, THAT YOU HAVE PAID DIRECTLY TO MICROCHIP FOR THIS 
+    SOFTWARE.
+*/
+
+#include "mcc_generated_files/mcc.h"
+#include "xlcd.h"
+#include <stdio.h>
+
+
+/*
+                         Main application
+ */
+
+        volatile unsigned char emerg_flag=0;
+        volatile unsigned char start_flag=0;
+        volatile unsigned char write_flag=0;    //ativada para escrita de start/ stop(apenas 1 vez))
+        volatile unsigned char flag_CNT1=0;
+        int valor=0;
+        char adc_flag=0;
+        //volatile int valor ;
+
+void emrgencia(void) {
+    if (INTCON2bits.INTEDG0 == 0) //flanco descendente(carrega no botão)
+    {
+        emerg_flag = 1; //a emergencia foi ativada
+        start_flag = 0; //desliga o sistema
+        INTCON2bits.INTEDG0 = 1; //muda configuração de flanco
+    } else //flanco ascendente(larga o botão)
+    {
+        emerg_flag = 0; //a emergencia foi desativada
+        INTCON2bits.INTEDG0 = 0; //muda configuração de flanco  
+    }
+    write_flag = 1;
+
+}
+
+void start(void) {
+ 
+        if (start_flag == 1) {
+            start_flag = 0; //sistema off
+        } else if (start_flag == 0) {
+            start_flag = 1; //sistema on
+        }
+        write_flag = 1;
+
+}
+
+void convert_timer (void){
+
+    ADC_StartConversion();
+
+}
+
+void CGRamAddr0(void)
+{
+     while (BusyXLCD());
+    /*
+     * Endereçamento da memória CGRAM:
+     * Primeira linha do 1º caracter,
+     * índice 0 = caracter 0 * 8 bytes por caracter
+     */
+
+    SetCGRamAddr(CARACTER_C);
+    while (BusyXLCD());
+    /*
+     * Definição do conteúdo do caracter, linha a linha
+     */
+    WriteDataXLCD(0b00000111); // linha 0
+    while (BusyXLCD());
+    WriteDataXLCD(0b00001000); // linha 1
+    while (BusyXLCD());
+    WriteDataXLCD(0b00010000); // linha 2
+    while (BusyXLCD());
+    WriteDataXLCD(0b00010000); // linha 3
+    while (BusyXLCD());
+    WriteDataXLCD(0b00010000); // linha 4
+    while (BusyXLCD());
+    WriteDataXLCD(0b00001000); // linha 5
+    while (BusyXLCD());
+    WriteDataXLCD(0b00010111); // linha 6
+    while (BusyXLCD());
+    WriteDataXLCD(0b00000000); // linha 7
+    while (BusyXLCD());
+}
+
+        
+
+void main(void)
+{
+        // Initialize the device
+        SYSTEM_Initialize();
+
+    
+        char name1[21] = "DAVID DRUMOND";
+        char name2[21] = "ADEOREL BANDEIRA";
+       // char project_title1[25] = "SISTEMA DE CONTROLO DE";
+       // char project_title2[40]= "UM PROCESSO DE INJECCAO";
+        int i;
+        int j;
+
+        
+               
+    //Inicialização do LCD:
+
+    
+    OpenXLCD(FOUR_BIT & LINES_5X7);
+    while (BusyXLCD());
+    WriteCmdXLCD(DON & CURSOR_OFF & BLINK_OFF);
+    while (BusyXLCD());
+    WriteCmdXLCD(SHIFT_CUR_RIGHT);
+    while (BusyXLCD());
+    WriteCmdXLCD(CLEAR_LCD);
+    while (BusyXLCD());
+    CGRamAddr0();   //carrega o caracter especial Ç
+    
+    WriteCmdXLCD(LINE1CLUN1);
+    while (BusyXLCD());
+    putsXLCD(name1);
+    while (BusyXLCD());
+    WriteCmdXLCD(LINE2CLUN1);
+    while (BusyXLCD()); 
+    putsXLCD(name2);
+    __delay_ms(500);  
+
+    WriteCmdXLCD(CLEAR_LCD);
+    while (BusyXLCD());
+
+    WriteCmdXLCD(LINE1CLUN1);
+    putsXLCD("SISTEMA DE CONTROLO DE");
+
+    while (BusyXLCD());
+    WriteCmdXLCD(LINE2CLUN1);
+    putsXLCD("UM PROCESSO DE INJE");
+    while (BusyXLCD());
+     putcXLCD(CARACTER_C);
+     while (BusyXLCD());
+     putsXLCD("AO");
+
+    for (i = 0; i < 10 ; i++ ) {
+
+    WriteCmdXLCD(SHIFT_DISP_LEFT);
+    while (BusyXLCD());
+    for (j = 0; j < 100 ; j++ )
+     __delay_ms(1);
+    }       
+            while (BusyXLCD());
+            WriteCmdXLCD(CLEAR_LCD);//clear LCD
+            
+    // If using interrupts in PIC18 High/Low Priority Mode you need to enable the Global High and Low Interrupts
+    // If using interrupts in PIC Mid-Range Compatibility Mode you need to enable the Global and Peripheral Interrupts
+    // Use the following macros to:
+
+    // Enable high priority global interrupts
+    INTERRUPT_GlobalInterruptHighEnable();
+    
+    // Enable low priority global interrupts.
+    INTERRUPT_GlobalInterruptLowEnable();
+
+    INT1_SetInterruptHandler(start); 
+    INT0_SetInterruptHandler(emrgencia);
+    
+    TMR1_SetInterruptHandler(convert_timer);
+    
+    //  ADC_SelectChannel(0);
+    //  ADC_StartConversion();
+            
+    // Disable high priority global interrupts
+    //INTERRUPT_GlobalInterruptHighDisable();
+
+    // Disable low priority global interrupts.
+    //INTERRUPT_GlobalInterruptLowDisable();
+
+    // Enable the Peripheral Interrupts
+    //INTERRUPT_PeripheralInterruptEnable();
+
+    // Disable the Peripheral Interrupts
+    //INTERRUPT_PeripheralInterruptDisable();
+
+    while (1) {
+        if (write_flag) {
+            write_flag = 0;
+            
+            while (BusyXLCD());
+            if (emerg_flag == 0) {
+                if (start_flag == 0) {
+                    WriteCmdXLCD(LINE1CLUN1);
+                    putsXLCD("SISTEMA OFF             ");
+
+                }
+                else {
+                    WriteCmdXLCD(0b10000000); //primeira linha/primeira coluna
+                    putsXLCD("SISTEMA ON               ");
+                }
+            }
+            else {
+                WriteCmdXLCD(0b10000000); //primeira linha/primeira coluna
+                while (BusyXLCD());
+                putsXLCD("EMERGENCIA ATIVADA            ");
+            }
+
+        }
+
+        if (adc_flag == 1) {
+            float angulo= 0.1759530792*valor;
+            char valor_analog[5];
+            sprintf(valor_analog,"Deslocamento= %.2f   ", angulo);
+            WriteCmdXLCD(LINE2CLUN1);
+            while (BusyXLCD());
+            
+            putsXLCD(valor_analog);
+            adc_flag=0;
+        }
+    }
+}
+
+
---
+++ mcc_generated_files/pin_manager.h
@@ -0,0 +1,267 @@
+/**
+  @Generated Pin Manager Header File
+
+  @Company:
+    Microchip Technology Inc.
+
+  @File Name:
+    pin_manager.h
+
+  @Summary:
+    This is the Pin Manager file generated using PIC10 / PIC12 / PIC16 / PIC18 MCUs
+
+  @Description
+    This header file provides APIs for driver for .
+    Generation Information :
+        Product Revision  :  PIC10 / PIC12 / PIC16 / PIC18 MCUs - 1.77
+        Device            :  PIC18F45K22
+        Driver Version    :  2.11
+    The generated drivers are tested against the following:
+        Compiler          :  XC8 2.05 and above
+        MPLAB 	          :  MPLAB X 5.20	
+*/
+
+/*
+    (c) 2018 Microchip Technology Inc. and its subsidiaries. 
+    
+    Subject to your compliance with these terms, you may use Microchip software and any 
+    derivatives exclusively with Microchip products. It is your responsibility to comply with third party 
+    license terms applicable to your use of third party software (including open source software) that 
+    may accompany Microchip software.
+    
+    THIS SOFTWARE IS SUPPLIED BY MICROCHIP "AS IS". NO WARRANTIES, WHETHER 
+    EXPRESS, IMPLIED OR STATUTORY, APPLY TO THIS SOFTWARE, INCLUDING ANY 
+    IMPLIED WARRANTIES OF NON-INFRINGEMENT, MERCHANTABILITY, AND FITNESS 
+    FOR A PARTICULAR PURPOSE.
+    
+    IN NO EVENT WILL MICROCHIP BE LIABLE FOR ANY INDIRECT, SPECIAL, PUNITIVE, 
+    INCIDENTAL OR CONSEQUENTIAL LOSS, DAMAGE, COST OR EXPENSE OF ANY KIND 
+    WHATSOEVER RELATED TO THE SOFTWARE, HOWEVER CAUSED, EVEN IF MICROCHIP 
+    HAS BEEN ADVISED OF THE POSSIBILITY OR THE DAMAGES ARE FORESEEABLE. TO 
+    THE FULLEST EXTENT ALLOWED BY LAW, MICROCHIP'S TOTAL LIABILITY ON ALL 
+    CLAIMS IN ANY WAY RELATED TO THIS SOFTWARE WILL NOT EXCEED THE AMOUNT 
+    OF FEES, IF ANY, THAT YOU HAVE PAID DIRECTLY TO MICROCHIP FOR THIS 
+    SOFTWARE.
+*/
+
+#ifndef PIN_MANAGER_H
+#define PIN_MANAGER_H
+
+/**
+  Section: Included Files
+*/
+
+#include <xc.h>
+
+#define INPUT   1
+#define OUTPUT  0
+
+#define HIGH    1
+#define LOW     0
+
+#define ANALOG      1
+#define DIGITAL     0
+
+#define PULL_UP_ENABLED      1
+#define PULL_UP_DISABLED     0
+
+// get/set channel_AN0 aliases
+#define channel_AN0_TRIS                 TRISAbits.TRISA0
+#define channel_AN0_LAT                  LATAbits.LATA0
+#define channel_AN0_PORT                 PORTAbits.RA0
+#define channel_AN0_ANS                  ANSELAbits.ANSA0
+#define channel_AN0_SetHigh()            do { LATAbits.LATA0 = 1; } while(0)
+#define channel_AN0_SetLow()             do { LATAbits.LATA0 = 0; } while(0)
+#define channel_AN0_Toggle()             do { LATAbits.LATA0 = ~LATAbits.LATA0; } while(0)
+#define channel_AN0_GetValue()           PORTAbits.RA0
+#define channel_AN0_SetDigitalInput()    do { TRISAbits.TRISA0 = 1; } while(0)
+#define channel_AN0_SetDigitalOutput()   do { TRISAbits.TRISA0 = 0; } while(0)
+#define channel_AN0_SetAnalogMode()      do { ANSELAbits.ANSA0 = 1; } while(0)
+#define channel_AN0_SetDigitalMode()     do { ANSELAbits.ANSA0 = 0; } while(0)
+
+// get/set RA2 procedures
+#define RA2_SetHigh()            do { LATAbits.LATA2 = 1; } while(0)
+#define RA2_SetLow()             do { LATAbits.LATA2 = 0; } while(0)
+#define RA2_Toggle()             do { LATAbits.LATA2 = ~LATAbits.LATA2; } while(0)
+#define RA2_GetValue()              PORTAbits.RA2
+#define RA2_SetDigitalInput()    do { TRISAbits.TRISA2 = 1; } while(0)
+#define RA2_SetDigitalOutput()   do { TRISAbits.TRISA2 = 0; } while(0)
+#define RA2_SetAnalogMode()         do { ANSELAbits.ANSA2 = 1; } while(0)
+#define RA2_SetDigitalMode()        do { ANSELAbits.ANSA2 = 0; } while(0)
+
+// get/set RA3 procedures
+#define RA3_SetHigh()            do { LATAbits.LATA3 = 1; } while(0)
+#define RA3_SetLow()             do { LATAbits.LATA3 = 0; } while(0)
+#define RA3_Toggle()             do { LATAbits.LATA3 = ~LATAbits.LATA3; } while(0)
+#define RA3_GetValue()              PORTAbits.RA3
+#define RA3_SetDigitalInput()    do { TRISAbits.TRISA3 = 1; } while(0)
+#define RA3_SetDigitalOutput()   do { TRISAbits.TRISA3 = 0; } while(0)
+#define RA3_SetAnalogMode()         do { ANSELAbits.ANSA3 = 1; } while(0)
+#define RA3_SetDigitalMode()        do { ANSELAbits.ANSA3 = 0; } while(0)
+
+// get/set RB0 procedures
+#define RB0_SetHigh()            do { LATBbits.LATB0 = 1; } while(0)
+#define RB0_SetLow()             do { LATBbits.LATB0 = 0; } while(0)
+#define RB0_Toggle()             do { LATBbits.LATB0 = ~LATBbits.LATB0; } while(0)
+#define RB0_GetValue()              PORTBbits.RB0
+#define RB0_SetDigitalInput()    do { TRISBbits.TRISB0 = 1; } while(0)
+#define RB0_SetDigitalOutput()   do { TRISBbits.TRISB0 = 0; } while(0)
+#define RB0_SetPullup()             do { WPUBbits.WPUB0 = 1; } while(0)
+#define RB0_ResetPullup()           do { WPUBbits.WPUB0 = 0; } while(0)
+#define RB0_SetAnalogMode()         do { ANSELBbits.ANSB0 = 1; } while(0)
+#define RB0_SetDigitalMode()        do { ANSELBbits.ANSB0 = 0; } while(0)
+
+// get/set RB1 procedures
+#define RB1_SetHigh()            do { LATBbits.LATB1 = 1; } while(0)
+#define RB1_SetLow()             do { LATBbits.LATB1 = 0; } while(0)
+#define RB1_Toggle()             do { LATBbits.LATB1 = ~LATBbits.LATB1; } while(0)
+#define RB1_GetValue()              PORTBbits.RB1
+#define RB1_SetDigitalInput()    do { TRISBbits.TRISB1 = 1; } while(0)
+#define RB1_SetDigitalOutput()   do { TRISBbits.TRISB1 = 0; } while(0)
+#define RB1_SetPullup()             do { WPUBbits.WPUB1 = 1; } while(0)
+#define RB1_ResetPullup()           do { WPUBbits.WPUB1 = 0; } while(0)
+#define RB1_SetAnalogMode()         do { ANSELBbits.ANSB1 = 1; } while(0)
+#define RB1_SetDigitalMode()        do { ANSELBbits.ANSB1 = 0; } while(0)
+
+// get/set IO_RD0 aliases
+#define IO_RD0_TRIS                 TRISDbits.TRISD0
+#define IO_RD0_LAT                  LATDbits.LATD0
+#define IO_RD0_PORT                 PORTDbits.RD0
+#define IO_RD0_ANS                  ANSELDbits.ANSD0
+#define IO_RD0_SetHigh()            do { LATDbits.LATD0 = 1; } while(0)
+#define IO_RD0_SetLow()             do { LATDbits.LATD0 = 0; } while(0)
+#define IO_RD0_Toggle()             do { LATDbits.LATD0 = ~LATDbits.LATD0; } while(0)
+#define IO_RD0_GetValue()           PORTDbits.RD0
+#define IO_RD0_SetDigitalInput()    do { TRISDbits.TRISD0 = 1; } while(0)
+#define IO_RD0_SetDigitalOutput()   do { TRISDbits.TRISD0 = 0; } while(0)
+#define IO_RD0_SetAnalogMode()      do { ANSELDbits.ANSD0 = 1; } while(0)
+#define IO_RD0_SetDigitalMode()     do { ANSELDbits.ANSD0 = 0; } while(0)
+
+// get/set IO_RD1 aliases
+#define IO_RD1_TRIS                 TRISDbits.TRISD1
+#define IO_RD1_LAT                  LATDbits.LATD1
+#define IO_RD1_PORT                 PORTDbits.RD1
+#define IO_RD1_ANS                  ANSELDbits.ANSD1
+#define IO_RD1_SetHigh()            do { LATDbits.LATD1 = 1; } while(0)
+#define IO_RD1_SetLow()             do { LATDbits.LATD1 = 0; } while(0)
+#define IO_RD1_Toggle()             do { LATDbits.LATD1 = ~LATDbits.LATD1; } while(0)
+#define IO_RD1_GetValue()           PORTDbits.RD1
+#define IO_RD1_SetDigitalInput()    do { TRISDbits.TRISD1 = 1; } while(0)
+#define IO_RD1_SetDigitalOutput()   do { TRISDbits.TRISD1 = 0; } while(0)
+#define IO_RD1_SetAnalogMode()      do { ANSELDbits.ANSD1 = 1; } while(0)
+#define IO_RD1_SetDigitalMode()     do { ANSELDbits.ANSD1 = 0; } while(0)
+
+// get/set IO_RD2 aliases
+#define IO_RD2_TRIS                 TRISDbits.TRISD2
+#define IO_RD2_LAT                  LATDbits.LATD2
+#define IO_RD2_PORT                 PORTDbits.RD2
+#define IO_RD2_ANS                  ANSELDbits.ANSD2
+#define IO_RD2_SetHigh()            do { LATDbits.LATD2 = 1; } while(0)
+#define IO_RD2_SetLow()             do { LATDbits.LATD2 = 0; } while(0)
+#define IO_RD2_Toggle()             do { LATDbits.LATD2 = ~LATDbits.LATD2; } while(0)
+#define IO_RD2_GetValue()           PORTDbits.RD2
+#define IO_RD2_SetDigitalInput()    do { TRISDbits.TRISD2 = 1; } while(0)
+#define IO_RD2_SetDigitalOutput()   do { TRISDbits.TRISD2 = 0; } while(0)
+#define IO_RD2_SetAnalogMode()      do { ANSELDbits.ANSD2 = 1; } while(0)
+#define IO_RD2_SetDigitalMode()     do { ANSELDbits.ANSD2 = 0; } while(0)
+
+// get/set IO_RD3 aliases
+#define IO_RD3_TRIS                 TRISDbits.TRISD3
+#define IO_RD3_LAT                  LATDbits.LATD3
+#define IO_RD3_PORT                 PORTDbits.RD3
+#define IO_RD3_ANS                  ANSELDbits.ANSD3
+#define IO_RD3_SetHigh()            do { LATDbits.LATD3 = 1; } while(0)
+#define IO_RD3_SetLow()             do { LATDbits.LATD3 = 0; } while(0)
+#define IO_RD3_Toggle()             do { LATDbits.LATD3 = ~LATDbits.LATD3; } while(0)
+#define IO_RD3_GetValue()           PORTDbits.RD3
+#define IO_RD3_SetDigitalInput()    do { TRISDbits.TRISD3 = 1; } while(0)
+#define IO_RD3_SetDigitalOutput()   do { TRISDbits.TRISD3 = 0; } while(0)
+#define IO_RD3_SetAnalogMode()      do { ANSELDbits.ANSD3 = 1; } while(0)
+#define IO_RD3_SetDigitalMode()     do { ANSELDbits.ANSD3 = 0; } while(0)
+
+// get/set IO_RD4 aliases
+#define IO_RD4_TRIS                 TRISDbits.TRISD4
+#define IO_RD4_LAT                  LATDbits.LATD4
+#define IO_RD4_PORT                 PORTDbits.RD4
+#define IO_RD4_ANS                  ANSELDbits.ANSD4
+#define IO_RD4_SetHigh()            do { LATDbits.LATD4 = 1; } while(0)
+#define IO_RD4_SetLow()             do { LATDbits.LATD4 = 0; } while(0)
+#define IO_RD4_Toggle()             do { LATDbits.LATD4 = ~LATDbits.LATD4; } while(0)
+#define IO_RD4_GetValue()           PORTDbits.RD4
+#define IO_RD4_SetDigitalInput()    do { TRISDbits.TRISD4 = 1; } while(0)
+#define IO_RD4_SetDigitalOutput()   do { TRISDbits.TRISD4 = 0; } while(0)
+#define IO_RD4_SetAnalogMode()      do { ANSELDbits.ANSD4 = 1; } while(0)
+#define IO_RD4_SetDigitalMode()     do { ANSELDbits.ANSD4 = 0; } while(0)
+
+// get/set IO_RD5 aliases
+#define IO_RD5_TRIS                 TRISDbits.TRISD5
+#define IO_RD5_LAT                  LATDbits.LATD5
+#define IO_RD5_PORT                 PORTDbits.RD5
+#define IO_RD5_ANS                  ANSELDbits.ANSD5
+#define IO_RD5_SetHigh()            do { LATDbits.LATD5 = 1; } while(0)
+#define IO_RD5_SetLow()             do { LATDbits.LATD5 = 0; } while(0)
+#define IO_RD5_Toggle()             do { LATDbits.LATD5 = ~LATDbits.LATD5; } while(0)
+#define IO_RD5_GetValue()           PORTDbits.RD5
+#define IO_RD5_SetDigitalInput()    do { TRISDbits.TRISD5 = 1; } while(0)
+#define IO_RD5_SetDigitalOutput()   do { TRISDbits.TRISD5 = 0; } while(0)
+#define IO_RD5_SetAnalogMode()      do { ANSELDbits.ANSD5 = 1; } while(0)
+#define IO_RD5_SetDigitalMode()     do { ANSELDbits.ANSD5 = 0; } while(0)
+
+// get/set IO_RD6 aliases
+#define IO_RD6_TRIS                 TRISDbits.TRISD6
+#define IO_RD6_LAT                  LATDbits.LATD6
+#define IO_RD6_PORT                 PORTDbits.RD6
+#define IO_RD6_ANS                  ANSELDbits.ANSD6
+#define IO_RD6_SetHigh()            do { LATDbits.LATD6 = 1; } while(0)
+#define IO_RD6_SetLow()             do { LATDbits.LATD6 = 0; } while(0)
+#define IO_RD6_Toggle()             do { LATDbits.LATD6 = ~LATDbits.LATD6; } while(0)
+#define IO_RD6_GetValue()           PORTDbits.RD6
+#define IO_RD6_SetDigitalInput()    do { TRISDbits.TRISD6 = 1; } while(0)
+#define IO_RD6_SetDigitalOutput()   do { TRISDbits.TRISD6 = 0; } while(0)
+#define IO_RD6_SetAnalogMode()      do { ANSELDbits.ANSD6 = 1; } while(0)
+#define IO_RD6_SetDigitalMode()     do { ANSELDbits.ANSD6 = 0; } while(0)
+
+// get/set IO_RD7 aliases
+#define IO_RD7_TRIS                 TRISDbits.TRISD7
+#define IO_RD7_LAT                  LATDbits.LATD7
+#define IO_RD7_PORT                 PORTDbits.RD7
+#define IO_RD7_ANS                  ANSELDbits.ANSD7
+#define IO_RD7_SetHigh()            do { LATDbits.LATD7 = 1; } while(0)
+#define IO_RD7_SetLow()             do { LATDbits.LATD7 = 0; } while(0)
+#define IO_RD7_Toggle()             do { LATDbits.LATD7 = ~LATDbits.LATD7; } while(0)
+#define IO_RD7_GetValue()           PORTDbits.RD7
+#define IO_RD7_SetDigitalInput()    do { TRISDbits.TRISD7 = 1; } while(0)
+#define IO_RD7_SetDigitalOutput()   do { TRISDbits.TRISD7 = 0; } while(0)
+#define IO_RD7_SetAnalogMode()      do { ANSELDbits.ANSD7 = 1; } while(0)
+#define IO_RD7_SetDigitalMode()     do { ANSELDbits.ANSD7 = 0; } while(0)
+
+/**
+   @Param
+    none
+   @Returns
+    none
+   @Description
+    GPIO and peripheral I/O initialization
+   @Example
+    PIN_MANAGER_Initialize();
+ */
+void PIN_MANAGER_Initialize (void);
+
+/**
+ * @Param
+    none
+ * @Returns
+    none
+ * @Description
+    Interrupt on Change Handling routine
+ * @Example
+    PIN_MANAGER_IOC();
+ */
+void PIN_MANAGER_IOC(void);
+
+
+
+#endif // PIN_MANAGER_H
+/**
+ End of File
+*/
